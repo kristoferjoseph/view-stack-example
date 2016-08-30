5a78e07f590e082abb3d6d96f393a7950bda2424
Kristofer Joseph <kristoferjoseph@gmail.com>
2016-08-30 15:46:07 -0700

    Update adds lasso.

--- server.js
+++ server.js
@@ -1,16 +1,56 @@
+var path = require('path')
 var express = require('express')
+var parser = require('body-parser')
+var layouts    = require('express-ejs-layouts')
+var compression = require('compression')
+var lasso = require('lasso')
+lasso.configure('./config.json')
+
 var app = express()
+app.use('/static', express.static('static'))
 app.set('view engine', 'ejs')
 app.set('views', './views')
-app.use(express.static('public'))
-app.use(function(req, res, next) {
-  res.blap = function(state) {
-    state = state || {}
-    state.initialState = JSON.stringify(state)
-    res.render('wrapper', state)
+app.use(parser.json({limit: '5mb'}))
+app.use(parser.urlencoded({extended:true}))
+
+app.use(
+  function(req, res, next) {
+    var head = 'nope'
+    var body = 'nope'
+
+    res.blap = function(state) {
+      state = state || {}
+      state.serverState = JSON.stringify(state)
+
+      lasso.lassoPage(
+        {
+          name: 'Begin',
+          dependencies: './browser.json'
+        },
+        function(err, result) {
+          if (err) {
+            console.error(err)
+            return
+          }
+          else {
+            head = result.getHeadHtml()
+            body = result.getBodyHtml()
+
+            state.head = head
+            state.body = body
+
+            res.render('wrapper', state)
+          }
+        }
+      )
+
+
+    }
+
+    next()
   }
-  next()
-})
+)
+
 
 app.get('/', function(req, res) {
   res.blap({title:'view-stack'})
